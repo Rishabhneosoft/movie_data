09654b51cbdb1bb99d094ad0ca567353bdf58f44
rishabh verma <rishabh.verma@neosoftmail.com>
2022-09-05 16:02:19 +0530

    add upload ods and excel file api

--- movie_app/views.py
+++ movie_app/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 # from rest_framework.generics import (ListAPIView, CreateAPIView, UpdateAPIView, DestroyAPIView)
 from rest_framework.generics import ListAPIView,DestroyAPIView,UpdateAPIView
 # from rest_framework.generics import ListAPIView
-
 import django_filters.rest_framework
 from django_filters.rest_framework import DjangoFilterBackend
 from .models import Movie
@@ -43,21 +42,42 @@ class SendExelView(APIView):
         excel_file = request.FILES["movie_data"]
         filename = fs.save(excel_file.name,excel_file) 
         file_url = settings.PROJECT_APPS + fs.url(filename) # project app url + filename
-        wb = xlrd.open_workbook(file_url)
-        sheet = wb.sheet_by_index(0) 
-        for i in range(sheet.nrows-1):
-            if i == sheet.nrows-1: # row-1 
-                break
-            name = sheet.cell_value(i+1,0)
-            actor = sheet.cell_value(i+1,1)
-            # import datetime 
-            release_date = sheet.cell_value(i+1,2)
-            rls_date = datetime.datetime(*xlrd.xldate_as_tuple(release_date, wb.datemode))
-            rls_date = rls_date.date()
-            poster_image = sheet.cell_value(i+1,3)
-            genres = sheet.cell_value(i+1,4)
-            date = sheet.cell_value(0,2).split('/')
-            move = Movie.objects.update_or_create(
+        file_name=filename.split(".")[-1]
+        if file_name == 'ods':
+        # wb = xlrd.open_workbook(file_url)
+            import pandas as pd
+            df=pd.read_excel('moviedata.ods', engine='odf')
+            print(df)
+
+            for index, row in df.iterrows():
+                name = row['name']
+                actors = row['actors']
+                release_date = row['release_date']
+                poster_image = row['poster_image']
+                geners = row['geners']
+                move = Movie.objects.update_or_create(
+                name=name,
+                actor=actors,
+                release_date=release_date,
+                poster_image=poster_image,
+                genres=geners,    
+            )
+        elif file_name == 'xlsx':
+            wb = xlrd.open_workbook(file_url)
+            sheet = wb.sheet_by_index(0) 
+            for i in range(sheet.nrows-1):
+                if i == sheet.nrows-1: # row-1 
+                    break
+                name = sheet.cell_value(i+1,0)
+                actor = sheet.cell_value(i+1,1)
+                # import datetime 
+                release_date = sheet.cell_value(i+1,2)
+                rls_date = datetime.datetime(*xlrd.xldate_as_tuple(release_date, wb.datemode))
+                rls_date = rls_date.date()
+                poster_image = sheet.cell_value(i+1,3)
+                genres = sheet.cell_value(i+1,4)
+                date = sheet.cell_value(0,2).split('/')
+                move = Movie.objects.update_or_create(
                 name=name,
                 actor=actor,
                 release_date=rls_date,
@@ -65,6 +85,7 @@ class SendExelView(APIView):
                 genres=genres,
                 
             )
+        else:
+            print("Please provide .ods or .xlsx file")
+            return Response('Please provide .ods or .xlsx file',status=status.HTTP_404_NOT_FOUND)
         return Response('File Uploaded Succesfully',status=status.HTTP_201_CREATED)
-
-        
